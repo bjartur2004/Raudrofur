89fd3b1eb0a02e6830a54a8b6d10f9b673e576fd
bjartur2004 <bjartur2004@gmail.com>
2020-11-03 16:47:57 +0000

    manual push

--- server -1.16.4 carpetmod/gitpush.bat
+++ server -1.16.4 carpetmod/gitpush.bat
@@ -1,3 +1,4 @@
+git pull
 git add ../.
 git commit -m "manual push"
 git push
--- server -1.16.4 carpetmod/start.bat
+++ server -1.16.4 carpetmod/start.bat
@@ -1,6 +1,7 @@
 java -Xms1g -Xmx1g -jar fabric-server-launch.jar nogui
 
 echo pushing to github
+git pull
 git add .
 git commit -m "server shutdown"
 git push

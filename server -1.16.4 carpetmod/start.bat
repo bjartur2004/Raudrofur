e8c55b54c2b0b385aae158dd2bba5e68c0150b84
bjartur2004 <bjartur2004@gmail.com>
2020-11-10 18:25:25 +0000

    server shutdown push

--- server -1.16.4 carpetmod/start.bat
+++ server -1.16.4 carpetmod/start.bat
@@ -1,8 +1,6 @@
-java -Xms2g -Xmx2g -jar fabric-server-launch.jar nogui
+java -Xms1g -Xmx2g -jar fabric-server-launch.jar nogui
 
-
-echo pushing to github
 git pull
 git add .
-git commit -m "server shutdown"
+git commit -m "server shutdown push"
 git push
